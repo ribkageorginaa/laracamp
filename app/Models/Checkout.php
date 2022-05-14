db33f3c58aa7c3bbd67ade54a151619054348187
unknown <ribkageorginaa@gmail.com>
2022-05-15 00:20:50 +0700

    Finish

---
+++ app/Models/Checkout.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Checkout extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['user_id', 'camp_id', 'payment_status', 'midtrans_url', 'midtrans_booking_code'];
+
+    public function setExpiredAttribute($value)
+    {
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
+    }
+
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
+
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
+
+
